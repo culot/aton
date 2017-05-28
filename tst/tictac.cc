dd1a7929a115ee7db35b92dd827d4f9255818581
culot <culot@FreeBSD.org>
2017-05-28 17:22:21 +0200

    Add a second test corresponding to the TIC-TAC use case

---
+++ tst/tictac.cc
@@ -0,0 +1,48 @@
+#include <string>
+#include <iostream>
+#include <unistd.h>
+#include <cassert>
+
+#include <zmq.h>
+
+static const int MAXREPLYSIZE = 1024;
+
+int main (void) {
+  std::cout << "Connecting to server... ";
+  void* context = zmq_ctx_new();
+  void* requester = zmq_socket(context, ZMQ_REQ);
+  zmq_connect(requester, "tcp://localhost:5555");
+  std::cout << "OK" << std::endl;
+
+  {
+    std::cout << "Sending 'tic' ... ";
+    zmq_send(requester, "tic", 3, 0);
+    char buf[MAXREPLYSIZE];
+    zmq_recv(requester, buf, MAXREPLYSIZE, 0);
+    std::string answer(buf);
+    assert(answer.length() == 0);
+    std::cout << "OK" << std::endl;
+  }
+  {
+    std::cout << "Sending 'tac' ... ";
+    zmq_send(requester, "tac", 3, 0);
+    char buf[MAXREPLYSIZE];
+    zmq_recv(requester, buf, MAXREPLYSIZE, 0);
+    std::string answer(buf);
+    assert(answer.length() == 0);
+    std::cout << "OK" << std::endl;
+  }
+  {
+    std::cout << "Sending 'tic' again ... ";
+    zmq_send(requester, "tic", 3, 0);
+    char buf[MAXREPLYSIZE];
+    zmq_recv(requester, buf, MAXREPLYSIZE, 0);
+    std::string answer(buf);
+    assert(answer == "tac");
+    std::cout << "OK, received 'tac'" << std::endl;
+  }
+
+  zmq_close(requester);
+  zmq_ctx_destroy(context);
+  return 0;
+}
