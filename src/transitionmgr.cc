3567722b18d6fce1fec125fbd93e466955eff0be
culot <culot@FreeBSD.org>
2017-06-01 14:10:24 +0200

    Implement transition registration

--- src/transitionmgr.cc
+++ src/transitionmgr.cc
@@ -7,22 +7,57 @@
 namespace aton {
 
 TransitionPtr TransitionMgr::registerTransition(const StatePtr& from, const StatePtr& to) {
+  if (!isValidTransition(from, to)) {
+    LOG(INFO) << __func__ << " - No transition to be created";
+    return nullptr;
+  } else {
+    TransitionPtr transition = getTransition(from, to);
+    if (transition == nullptr) {
+      LOG(INFO) << __func__ << " - Transition must be created";
+      return createTransition(from, to);
+    } else {
+      LOG(INFO) << __func__ << " - Transition already exists";
+      increaseTransitionWeight(transition);
+      return transition;
+    }
+  }
+}
+
+TransitionPtr TransitionMgr::createTransition(const StatePtr& from, const StatePtr& to) {
+  LOG(INFO) << __func__ << " - Creating new transition from ["
+            << *from << "] to [" << *to << "]";
+  TransitionPtr transition = std::make_shared<Transition>(from, to);
+  transitions_.emplace(from->signature(), transition);
+  return transition;
+}
+
+TransitionPtr TransitionMgr::getTransition(const StatePtr& from, const StatePtr& to) {
   uint64_t fromStateSig = from->signature();
   // For now, just extract the first item of the multimap (use equal_range to get all items)
   auto it = transitions_.find(fromStateSig);
-  TransitionPtr transition;
   if (it == transitions_.end()) {
-    LOG(INFO) << __func__ << " - Creating new transition from ["
-              << *from << "] to [" << *to << "]";
-    transition = std::make_shared<Transition>(from, to);
-    transitions_.emplace(fromStateSig, transition);
+    LOG(INFO) << __func__ << "Transition from [" << *from << "] to [" << *to
+              << "] does not exist";
+    return nullptr;
   } else {
-    transition = it->second;
-    transition->increaseWeight();
-    LOG(INFO) << __func__ << " - Weigth increased (now [" << transition->weight()
-              << "]) for transition from [" << *from << "] to [" << *to << "]";
+    TransitionPtr transition = it->second;
+    return transition;
   }
-  return transition;
+}
+
+void TransitionMgr::increaseTransitionWeight(TransitionPtr& transition) const {
+  transition->increaseWeight();
+  LOG(INFO) << __func__ << " - Weigth increased (now [" << transition->weight()
+            << "]) for transition from [" << *transition->from()
+            << "] to [" << *transition->to() << "]";
+}
+
+bool TransitionMgr::isValidTransition(const StatePtr& from, const StatePtr& to) const {
+  if (from == nullptr || to == nullptr) {
+    LOG(INFO) << "Transition misses either FROM or TO state";
+    return false;
+  }
+  return true;
 }
 
 }
--- src/transitionmgr.h
+++ src/transitionmgr.h
@@ -12,6 +12,11 @@ class TransitionMgr {
   TransitionPtr registerTransition(const StatePtr& from, const StatePtr& to);
 
  private:
+  bool isValidTransition(const StatePtr& from, const StatePtr& to) const;
+  TransitionPtr getTransition(const StatePtr& from, const StatePtr& to);
+  TransitionPtr createTransition(const StatePtr& from, const StatePtr& to);
+  void increaseTransitionWeight(TransitionPtr& transition) const;
+
   std::multimap<uint64_t, TransitionPtr> transitions_;
 };
 
