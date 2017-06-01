0c9646880ad855c437e5f6b1eb1b7574cf55534f
culot <culot@FreeBSD.org>
2017-06-01 21:44:50 +0200

    Server now returns a prediction of the next possible state
    The server now returns the final state of the most probable
    transition, starting from the state provided as input.

--- src/server.cc
+++ src/server.cc
@@ -48,7 +48,17 @@ std::string Server::handleRequest(const std::string& request) {
   StatePtr currentState = statemgr_.currentTextState();
   StatePtr newState = statemgr_.registerTextState(request);
   transitionmgr_.registerTransition(currentState, newState);
-  return "";
+
+  // Now for the answer, look for the most probable transition
+  // starting with newState, and if it exists then return its
+  // final state.
+  TransitionPtr transition = transitionmgr_.getTransitionFrom(newState);
+  if (transition != nullptr) {
+    StatePtr mostProbableNextState = transition->to();
+    return mostProbableNextState->str();
+  } else {
+    return "";
+  }
 }
 
 std::string Server::getStatus() const {
