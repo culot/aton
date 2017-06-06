2cfd3068020810a6a28303e808d4d39639049d64
culot <culot@FreeBSD.org>
2017-06-06 18:20:33 +0200

    Add helper to request a clearing of the server memory

--- tst/common.cc
+++ tst/common.cc
@@ -6,6 +6,16 @@
 
 namespace common {
 
+void clearServerMemory(void* tgt) {
+  static const std::string clear("CLEAR");
+  std::cout << "Clearing server memory ... ";
+  zmq_send(tgt, clear.c_str(), clear.length(), 0);
+  char buf[MAXREPLYSIZE];
+  zmq_recv(tgt, buf, MAXREPLYSIZE, 0);
+  std::string answer(buf);
+  std::cout << answer << std::endl;
+}
+
 void sendAndReceive(void* tgt, const std::string& in, const std::string& out) {
   std::cout << "Sending '" << in << "' ... ";
   zmq_send(tgt, in.c_str(), in.length(), 0);
--- tst/common.h
+++ tst/common.h
@@ -6,6 +6,7 @@ namespace common {
 
 static const int MAXREPLYSIZE = 1024;
 
+void clearServerMemory(void* tgt);
 void sendAndReceive(void* tgt, const std::string& in, const std::string& out);
 
 }
