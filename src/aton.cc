1a22145dc5187609f01bf091edc48f5df9c850c8
culot <culot@FreeBSD.org>
2017-06-07 18:46:00 +0200

    Add basic opening/closing of storage from main process

--- src/aton.cc
+++ src/aton.cc
@@ -2,15 +2,32 @@
 #include <iostream>
 #include <unistd.h>
 #include <cassert>
+#include <csignal>
 
 #include <glog/logging.h>
 
+#include "storage.h"
 #include "server.h"
 
 using namespace aton;
 
+void sigHandler(int signal) {
+  LOG(INFO) << "Received signal [" << signal << "]";
+  Storage::instance().save();
+  Storage::instance().close();
+}
+
 int main(int argc, char** argv) {
   google::InitGoogleLogging(argv[0]);
+
+  std::signal(SIGINT, sigHandler);
+
+  try {
+    Storage::instance().load();
+  } catch (const std::exception& e) {
+    LOG(ERROR) << "Aborting due to following error: " << e.what();
+    return 1;
+  }
 
   Server srv;
   try {
