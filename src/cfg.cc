46aa09468e8e05091778b3aea81f0967a7413037
culot <culot@FreeBSD.org>
2017-06-13 18:24:04 +0200

    Add new JUNCTURE keyword
    This new keyword will be used to denote the end of a series of
    phonemes and serve as a boundary between words.

--- src/cfg.cc
+++ src/cfg.cc
@@ -8,6 +8,7 @@ const int MAXINPUTSIZE = 1024;
 
 const std::string REQUEST_STATUS("STATUS");
 const std::string REQUEST_CLEAR("CLEAR");
+const std::string REQUEST_JUNCTURE("JUNCTURE");
 const std::string DB_FILE("/var/tmp/aton.db");
 
 }
