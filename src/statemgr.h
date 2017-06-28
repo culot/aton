9568152963cc80142c59792b944376eeada1c36c
culot <culot@FreeBSD.org>
2017-06-28 22:33:19 +0200

    Add a helper to get the number of states recorded by the statemgr

--- src/statemgr.h
+++ src/statemgr.h
@@ -15,6 +15,7 @@ namespace aton {
 class StateMgr {
  public:
   void clear();
+  int size() const {return states_.size();}
   StatePtr registerState(State::Type type, const std::string& input, uint64_t id = 0);
   TransitionPtr registerTransition(uint64_t from, uint64_t to, int weight);
   std::vector<StatePtr> getAllStates() const;
