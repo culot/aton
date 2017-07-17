66d70bde7385bad15a2b5e4737f71472e79f8259
culot <culot@FreeBSD.org>
2017-07-17 18:14:17 +0200

    Add new method to send data to server without checking answer

--- tst/common.h
+++ tst/common.h
@@ -7,6 +7,7 @@ namespace common {
 static const int MAXREPLYSIZE = 1024;
 
 void clearServerMemory(void* tgt);
+std::string send(void* tgt, const std::string& in);
 void sendAndReceive(void* tgt, const std::string& in, const std::string& out);
 void sendJuncture(void* tgt);
 
