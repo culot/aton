e0a34e4d8186aa352a4ae53dc5e3c6aced128467
culot <culot@FreeBSD.org>
2017-07-19 21:02:24 +0200

    Define distinct file names for plots with or without pictures

--- src/cfg.h
+++ src/cfg.h
@@ -13,6 +13,7 @@ extern const std::string REQUEST_CLEAR;
 extern const std::string REQUEST_JUNCTURE;
 extern const std::string DB_FILE;
 extern const std::string PLOT_FILE_GRAPHVIZ;
+extern const std::string PLOT_FILE_GRAPHVIZ_PICS;
 
 }
 }
